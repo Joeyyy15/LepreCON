9dec07aba659124ed02fbb4b2ecac065433ae935
Joseph Newville <joenewvb@gmail.com>
2026-03-11 17:27:35 -0500

    Renamed project from bucksgame to LepreCON

--- LepreCON/LepreCONApp.swift
+++ LepreCON/LepreCONApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 @main
-struct bucksgameApp: App {
+struct LepreCONApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
