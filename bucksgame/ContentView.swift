64fa334fe42f7b866de684e08b652e5ccaa82944
Joseph Newville <joenewvb@gmail.com>
2026-03-11 16:30:47 -0500

    Initial iOS app setup

---
+++ bucksgame/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  bucksgame
+//
+//  Created by Joey Newville on 3/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        VStack {
+            Text("Joey's app is working")
+                .font(.title)
+
+            Text("My first iPhone app")
+        }
+        .padding()
+    }
+
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
